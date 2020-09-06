76f463eda509dd570c8203e04a4d93f7237c2b33
EckhardtLuke <59931627+EckhardtLuke@users.noreply.github.com>
2020-09-06 15:16:48 -0500

    neato

--- p5/Unit1/images/sketch.js
+++ p5/Unit1/images/sketch.js
@@ -6,12 +6,13 @@ function setup() {
 
   img1 = loadImage("assets/img1.jpg");
   img2 = loadImage("assets/img2.jpg");
-  img2 = loadImage("assets/img3.jpg");
+  img3 = loadImage("assets/img3.jpg");
 }
 
 function draw() {
+
   image(img1, width/2, height/2, 200, 200);
-  image(img2, width/2, height/2 - 210, 200, 200);
-  image(img3, width/2, height/2 + 210, 200, 200);
+  image(img2, width/2, height/2 + 210, 200, 200);
+  image(img3, width/2, height/2 - 210, 200, 200);
 
 }
