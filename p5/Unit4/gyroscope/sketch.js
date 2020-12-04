02350b30887147ebcfe2337b7476ae786a4ca4c9
EckhardtLuke <59931627+EckhardtLuke@users.noreply.github.com>
2020-12-04 17:29:40 -0600

    g

--- p5/Unit4/gyroscope/sketch.js
+++ p5/Unit4/gyroscope/sketch.js
@@ -60,7 +60,7 @@ function draw() {
   //  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
 
   // draw the FROG
-  image(bunnyImage, 0, 0, 500, 500);
+  image(bunnyImage, 0, 0, 80, 80);
   //fill('green');
   //ellipse(0, 0, 80, 80);
   pop();
